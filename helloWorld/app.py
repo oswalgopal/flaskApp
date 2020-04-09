378f2ca556c25b65348f27d3d81dae8e384b9fe4
Gopal Oswal <gopal@kritin.in>
2020-04-09 10:24:55 +0530

    added basic comments

--- helloWorld/app.py
+++ helloWorld/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask #importing Flask from flask
 
-app = Flask(__name__)
+app = Flask('__main__') # creating an app with the help of the class named as Flask
 
+# from the app we are defining the routes by default we are having '/' route
 @app.route('/')
-def hello_world():
-    return 'Hello, World'
+def hello_world(): # defining a function to be called when that route url is requested
+    return 'Hello, World' # returning a hello world.
+
+# handling the error for the routes
+@app.errorhandler(404)
+def page_not_found(error): # defining a function to be called when that route url is requested
+    return 'Page not found' # returning a page not found.
